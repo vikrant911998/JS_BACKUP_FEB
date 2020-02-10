305c931c02e64cf98881e4f4a72f6d7e2a6c12ae
vikrant bhadauria <vikrantbhadauriya2@gmail.com>
2020-02-10 15:33:00 +0530

    Add files via upload

---
+++ demo1/index.js
@@ -0,0 +1,17 @@
+
+// temp = document.getElementsByTagName('h1')[0];
+
+// temp = document.getElementById('h_1');
+temp = document.querySelector('#h_1');
+
+// temp = $('h_1');
+
+console.log(temp);
+
+temp.style.color = 'white';
+temp.style.backgroundColor = 'black';
+temp.style.fontFamily = 'Segoe UI, Tahoma, Geneva, Verdana, sans-serif';
+temp.style.fontSize = '50px';
+temp.style.padding = '20px';
+temp.style.width = '50%';
+temp.style.height = '700px';
